276ba03f3dc30f5e7e58cb34a07c1ce41b8f9c22
S4R1X <seth@bytech.ca>
2026-09-07 23:29:40 +0200

    Followed turorials

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1223,9 +1223,9 @@
 #define FLAG_ITEM_SAFARI_ZONE_SOUTH_EAST_BIG_PEARL                  0x492
 
 #define FLAG_UNUSED_0x493                                           0x493 // Unused Flag
-#define FLAG_UNUSED_0x494                                           0x494 // Unused Flag
-#define FLAG_UNUSED_0x495                                           0x495 // Unused Flag
-#define FLAG_UNUSED_0x496                                           0x496 // Unused Flag
+#define FLAG_SELECTED_JANGMO_O                                      0x494 // Unused Flag
+#define FLAG_SELECTED_FLABEBE                                       0x495 // Unused Flag
+#define FLAG_SELECTED_ARON                                          0x496 // Unused Flag
 #define FLAG_UNUSED_0x497                                           0x497 // Unused Flag
 #define FLAG_UNUSED_0x498                                           0x498 // Unused Flag
 #define FLAG_UNUSED_0x499                                           0x499 // Unused Flag
