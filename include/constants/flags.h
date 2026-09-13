ba59b6ec873ae1dffe3c248883eab46152f34dd4
S4R1X <seth@bytech.ca>
2026-09-13 18:56:58 +0200

    Added sketchy rat guy and fixed tile set errors

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1226,7 +1226,7 @@
 #define FLAG_SELECTED_JANGMO_O                                      0x494 // Unused Flag
 #define FLAG_SELECTED_FLABEBE                                       0x495 // Unused Flag
 #define FLAG_SELECTED_ARON                                          0x496 // Unused Flag
-#define FLAG_UNUSED_0x497                                           0x497 // Unused Flag
+#define FLAG_RECIEVED_WIERD_RAT                                     0x497 // Unused Flag
 #define FLAG_UNUSED_0x498                                           0x498 // Unused Flag
 #define FLAG_UNUSED_0x499                                           0x499 // Unused Flag
 #define FLAG_UNUSED_0x49A                                           0x49A // Unused Flag
